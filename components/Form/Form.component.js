a760a5987201ed1efa11e9a77c18d454212f5069
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-05-25 19:27:57 -0300

    Form component

---
+++ components/Form/Form.component.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import Input from '../Input/input.component';
+import Button from '../Button/Button.component';
+import {
+  StyledForm,
+  ErrorField,
+  StyledInputLabel,
+  StyledLabel,
+} from './Form.styled'
+
+const Form = ({ onFormSubmit, config = [], buttonLabel = 'Agregar' }) => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = data => {
+    onFormSubmit(data);
+  }
+
+  return (
+    <StyledForm onSubmit={handleSubmit(onSubmit)}>
+      { config.map(({ label, type, name, placeholder, validations, textValidation }) => (
+        <StyledInputLabel key={name}>
+          <StyledLabel>{label}</StyledLabel>
+          <Input
+            type={type}
+            name={name}
+            placeholder={placeholder}
+            {...register(name, { ...validations })}
+          />
+          {errors[name] && <ErrorField>{textValidation}</ErrorField>}
+        </StyledInputLabel>
+      ))}
+      <Button type="submit" label={buttonLabel} />
+    </StyledForm>
+  )
+}
+
+export default Form
