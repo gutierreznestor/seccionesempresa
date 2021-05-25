79c00737ca1043d5a49bc4d6a55c8b36ee332770
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-05-25 19:28:06 -0300

    Using form component

--- pages/secciones-empresa/new/index.js
+++ pages/secciones-empresa/new/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import Form from '../../../components/Form/Form.component';
+import Input from '../../../components/Input/input.component';
+
 import Layout from '../../../components/Layout';
 
+const AgregarSeccionForm = [
+  {
+    label: 'Nombre',
+    type: 'text',
+    name: 'Nombre',
+    placeholder: 'Producción',
+    validations: { required: true },
+    textValidation: 'Este campo es requerido.',
+  },
+];
+
 const NuevaSeccion = () => {
+
+  const onSubmit = data => {
+    console.log({ page: data })
+  };
   return (
     <Layout title='Nueva sección'>
       <h1>Nueva sección</h1>
+      <Form onFormSubmit={onSubmit} config={AgregarSeccionForm} />
     </Layout>
   )
 }
