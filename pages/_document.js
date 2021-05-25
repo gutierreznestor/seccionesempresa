992b36685c37c50b3e3d234dd2a256c38358eb14
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-05-25 00:30:37 -0300

    Html tag on _document

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-import flush from 'styled-jsx/server'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import flush from 'styled-jsx/server';
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
@@ -10,9 +10,8 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <Html>
         <Head>
-          <title>Proyecto 1 - Gutierrez</title>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
         </Head>
         <body className="custom_class">
@@ -20,7 +19,7 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
