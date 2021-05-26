802d05261e0826b02ad57256856d09d53460a5b6
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-05-26 10:26:05 -0300

    Edit page

---
+++ pages/secciones-empresa/edit/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Router from 'next/router'
+
+import Form from '../../../components/Form/Form.component';
+import Layout from '../../../components/Layout';
+
+const EditarSeccionForm = [
+  {
+    label: 'Nombre',
+    type: 'text',
+    name: 'Nombre',
+    placeholder: 'Producción',
+    validations: { required: true },
+    textValidation: 'Este campo es requerido.',
+    value: 'asdasd',
+  },
+];
+
+const EditarSeccion = () => {
+
+  const onSubmit = async (data) => {
+    const { Nombre } = data;
+    try {
+      const res = await fetch('/api/seccionesEmpresa/new-secciones-empresa', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          Nombre,
+        }),
+      })
+      const json = await res.json()
+      if (!res.ok) throw Error(json.message)
+      Router.push('/')
+    } catch (e) {
+      throw Error(e.message)
+    }
+  }
+
+  return (
+    <Layout title='Editar sección'>
+      <h1>Editar sección</h1>
+      <Form
+        onFormSubmit={onSubmit}
+        config={EditarSeccionForm}
+        defaultValues={{ Nombre: 'Test' }} />
+    </Layout>
+  )
+}
+
+export default EditarSeccion;
