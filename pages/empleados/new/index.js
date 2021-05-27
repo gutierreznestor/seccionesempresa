4e2de24908ba0b41291d2a3778d55bfc52979527
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-05-26 22:23:16 -0300

    New empleado page

---
+++ pages/empleados/new/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Router from 'next/router'
+
+import Form from '../../../components/Form/Form.component';
+import Layout from '../../../components/Layout';
+import { nuevaSeccionEmpresa } from '../../../services/seccionesEmpresa.service';
+import { nuevoEmpleado } from '../../../services/empleados.service';
+
+const NuevoEmpleadoForm = [
+  {
+    label: 'Nombre',
+    type: 'text',
+    name: 'Nombre',
+    placeholder: 'Juan',
+    validations: { required: true },
+    textValidation: 'Este campo es requerido.',
+  },
+  {
+    label: 'Apellido',
+    type: 'text',
+    name: 'Apellido',
+    placeholder: 'Dow',
+    validations: { required: true },
+    textValidation: 'Este campo es requerido.',
+  },
+  {
+    label: 'Sección empresa',
+    type: 'number',
+    name: 'idSeccionEmpresa',
+    placeholder: '123',
+    validations: { required: true },
+    textValidation: 'Este campo es requerido.',
+  },
+];
+
+const NuevaSeccion = () => {
+
+  const onSubmit = async (data) => {
+    const { Nombre, Apellido, idSeccionEmpresa } = data;
+    try {
+      const res = await nuevoEmpleado({ Nombre, Apellido, idSeccionEmpresa })
+      const json = await res.json()
+      if (!res.ok) throw Error(json.message)
+      Router.push('/empleados')
+    } catch (e) {
+      throw Error(e.message)
+    }
+  }
+
+  return (
+    <Layout title='Nuevo empleado'>
+      <h1>Nuevo empleado</h1>
+      <Form onFormSubmit={onSubmit} config={NuevoEmpleadoForm} />
+    </Layout>
+  )
+}
+
+export default NuevaSeccion;
