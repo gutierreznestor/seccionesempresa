e968c818bc66b5cbef4d5173a9160c48a3c3f51e
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-05-26 22:03:19 -0300

    empelados service

---
+++ services/empleados.service.js
@@ -0,0 +1,54 @@
+export const getEmpleados = async () => {
+  const res = await fetch('/api/empleados/get-empleados', {
+    method: 'GET',
+  });
+  return res;
+}
+
+export const deleteEmpleado = async (idEmpleado) => {
+  const res = await fetch('/api/empleados/delete-empleado', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      idEmpleado,
+    }),
+  });
+  return res;
+}
+
+export const getEmpleado = async (id) => {
+  const res = await fetch(`/api/empleados/get-empleado?id=${id}`, {
+    method: 'GET',
+  });
+  return res;
+}
+
+export const editarEmpleado = async ({ id, Nombre = '' }) => {
+  const url = `/api/empleados/edit-empleado?id=${id}`;
+  const res = await fetch(url, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      Nombre,
+    }),
+  });
+  return res;
+}
+
+export const nuevoEmpleado = async ({ Nombre = '' }) => {
+  const url = `/api/empleados/new-empleado`;
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      Nombre,
+    }),
+  });
+  return res;
+}
