c43f342b8e83f5d6ed9709251dc47908dbec5b53
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-15 00:01:05 -0300

    usuarios page

---
+++ pages/usuarios/index.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import Router from 'next/router';
+
+import Layout from '../../components/Layout';
+import AppLink from '../../components/AppLink/AppLink.component';
+import EmpleadosList from '../../components/EmpleadosList';
+import { deleteUsuario, getUsuarios } from '../../services/usuarios.service';
+import ErrorMessage from '../../components/ErrorMessage/ErrorMessage.component';
+
+const Usuarios = () => {
+  const [loading, setLoading] = useState(false);
+  const [list, setList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchUsuarios = async () => {
+    setLoading(true);
+    const res = await getUsuarios();
+    setLoading(false)
+    const data = await res.json()
+    setList(data);
+  }
+
+  useEffect(() => {
+    fetchUsuarios();
+    return () => {
+      setErrorMessage('');
+    }
+  }, []);
+
+  const onDelete = async (id) => {
+    const ok = confirm('¿Quieres eliminar al usuario?');
+    if (ok) {
+      setLoading(true);
+      const res = await deleteUsuario(id);
+      const json = await res.json();
+      if (!res.ok) setErrorMessage(json.message);
+      fetchUsuarios();
+      Router.push('usuarios');
+    }
+  }
+
+  return (
+    <Layout title='Usuarios'>
+      <h1>Usuarios</h1>
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+      <AppLink href='/usuarios/new' title='Nuevo usuario' />
+      {loading ?
+        <span>Cargando...</span> :
+        <EmpleadosList
+          list={list}
+          onDelete={onDelete}
+        />}
+    </Layout>
+  )
+}
+
+export default Usuarios;
