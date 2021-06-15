d930d95263c379d22b8954b8ecdc7e850eb3e5a2
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-15 20:17:25 -0300

    get-usuarios api query

--- pages/api/usuarios/get-usuarios.js
+++ pages/api/usuarios/get-usuarios.js
@@ -3,13 +3,14 @@ import { query } from '../../../lib/db'
 const handler = async (_, res) => {
   try {
     const results = await query(`
-    SELECT DISTINCT usuarios.Nombre, usuarios.Apellido, 
+    SELECT DISTINCT usuarios.idUsuario, usuarios.Nombre, usuarios.Apellido, 
       up.idUsuarioPerfil, usuarios.Usuario, perfiles.Nombre AS Perfil 
     FROM usuarios
     INNER JOIN usuarios_tiene_perfiles AS up ON 
       (up.idUsuario = usuarios.idUsuario)
     INNER JOIN perfiles ON
-      (perfiles.idPerfil = up.idPerfil)  
+      (perfiles.idPerfil = up.idPerfil)
+    GROUP BY usuarios.idUsuario
     `);
 
     return res.json(results)
