be3a30dbed619369bbed6c5b8730cbb4228cab34
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-15 20:17:50 -0300

    fix seccionesEmpresaList import on secciones empresa page

--- pages/secciones-empresa/index.js
+++ pages/secciones-empresa/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Router from 'next/router';
 
 import Layout from '../../components/Layout';
-import SeccionesEmpresaList from '../../components/SeccionesEmpresaList';
+import SeccionesEmpresaList from '../../components/SeccionesEmpresaList/SeccionesEmpresaList.component'
 import AppLink from '../../components/AppLink/AppLink.component';
 import { deleteSeccionesEmpresa, getSeccionesEmpresa } from '../../services/seccionesEmpresa.service';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage.component';
@@ -53,8 +53,8 @@ const SeccionesEmpresa = () => {
     <Layout title='Secciones empresa'>
       <h1>Secciones empresa</h1>
       <AppLink href='/secciones-empresa/new' title='Nueva sección' />
-      { errorMessage && <ErrorMessage message={errorMessage} />}
-      { loading ?
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+      {loading ?
         <span>Cargando...</span> :
         <SeccionesEmpresaList
           list={list}
