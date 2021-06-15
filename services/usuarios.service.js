235f4425c7d19feef129c5b25212af295bfb416a
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-14 23:55:48 -0300

    usuarios service

---
+++ services/usuarios.service.js
@@ -0,0 +1,58 @@
+export const getUsuarios = async () => {
+  const res = await fetch('/api/usuarios/get-usuarios', {
+    method: 'GET',
+  });
+  return res;
+}
+
+export const deleteUsuario = async (id) => {
+  const res = await fetch('/api/usuarios/delete-usuario', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      id,
+    }),
+  });
+  return res;
+}
+
+export const getUsuario = async (id) => {
+  const res = await fetch(`/api/usuarios/get-usuario?id=${id}`, {
+    method: 'GET',
+  });
+  return res;
+}
+
+export const editarUsuario = async ({ id, Nombre = '', Apellido = '' }) => {
+  const url = `/api/usuarios/edit-usuario?id=${id}`;
+  const res = await fetch(url, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      Nombre,
+      Apellido,
+    }),
+  });
+  return res;
+}
+
+export const nuevoUsuario = async ({ Usuario, Nombre, Apellido, Password }) => {
+  const url = `/api/usuarios/new-usuario`;
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      Usuario,
+      Nombre,
+      Apellido,
+      Password,
+    }),
+  });
+  return res;
+}
