c6d44e03b8b00c7b44d4a3cf070f5ccc5329c5b1
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-15 23:24:51 -0300

    new perfil api

---
+++ pages/api/perfiles/new-perfil.js
@@ -0,0 +1,25 @@
+import { query } from '../../../lib/db'
+
+const handler = async (req, res) => {
+  const { Nombre } = req.body
+  try {
+    if (!Nombre) {
+      return res
+        .status(400)
+        .json({ message: 'Se requiere el Nombre en el cuerpo de la llamada.' })
+    }
+    const results = await query(
+      `
+      INSERT INTO perfiles (Nombre) 
+      VALUES (?)
+      `,
+      [Nombre],
+    )
+
+    return res.json(results)
+  } catch (e) {
+    res.status(500).json({ message: e.message })
+  }
+}
+
+export default handler
