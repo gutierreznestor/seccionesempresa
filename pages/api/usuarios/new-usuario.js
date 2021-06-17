9d6f6ccf52dcae932cee01aa5211ed4110de85dc
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-17 11:49:24 -0300

    Store hash password

--- pages/api/usuarios/new-usuario.js
+++ pages/api/usuarios/new-usuario.js
@@ -1,4 +1,5 @@
-import { query } from '../../../lib/db'
+import { hash } from 'bcrypt';
+import { query } from '../../../lib/db';
 
 const handler = async (req, res) => {
   const { Usuario, Nombre, Apellido, Password } = req.body;
@@ -8,12 +9,13 @@ const handler = async (req, res) => {
         .status(400)
         .json({ message: 'Complete todos los campos.' })
     }
+    const hashPassword = await hash(Password, 10);
     const results = await query(
       `
       INSERT INTO usuarios (Usuario, Nombre, Apellido, Password) 
       VALUES (?, ?, ?, ?);
       `,
-      [Usuario, Nombre, Apellido, Password],
+      [Usuario, Nombre, Apellido, hashPassword],
     );
 
     return res.json(results)
