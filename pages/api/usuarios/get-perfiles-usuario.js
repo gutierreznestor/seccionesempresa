c008b375e16560993f5d1a6da456de152000f864
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-18 19:19:42 -0300

    get perfiles usuario api

---
+++ pages/api/usuarios/get-perfiles-usuario.js
@@ -0,0 +1,24 @@
+import { query } from '../../../lib/db'
+
+const handler = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const results = await query(`
+      SELECT DISTINCT up.idUsuarioPerfil, usuarios.idUsuario, usuarios.Usuario, 
+        perfiles.Nombre AS Perfil, usuarios.Nombre, usuarios.Apellido 
+      FROM usuarios
+      INNER JOIN usuarios_tiene_perfiles AS up ON 
+        (up.idUsuario = usuarios.idUsuario)
+      INNER JOIN perfiles ON
+        (perfiles.idPerfil = up.idPerfil)
+      WHERE usuarios.idUsuario=?
+      `,
+      [id],
+    );
+    return res.json(results)
+  } catch (e) {
+    res.status(500).json({ message: e.message })
+  }
+}
+
+export default handler
