2190aca20ba899ea5d738201600a57a5e6f33031
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-19 20:53:38 -0300

    Perfiles usuarios added to view user

--- pages/usuarios/[id]/index.js
+++ pages/usuarios/[id]/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
+import { getPerfilesUsuario, getUsuario } from '../../../services/usuarios.service';
 import Layout from '../../../components/Layout';
-import { getUsuario } from '../../../services/usuarios.service';
 import { FieldContainer } from './ViewUser.styled';
 import ErrorMessage from '../../../components/ErrorMessage/ErrorMessage.component';
+import PerfilesUsuarioList from '../../../components/PerfilesUsuarioList/PerfilesUsuarioList.component';
+import AppLink from '../../../components/AppLink/AppLink.component';
 
 const ListItem = ({ title, description }) => (
   <FieldContainer>
@@ -18,37 +20,61 @@ const ViewUser = () => {
   const [values, setValues] = useState({});
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [perfiles, setPerfiles] = useState([])
 
-  const getData = async (id) => {
-    setErrorMessage('');
-    setLoading(true);
-    const res = await getUsuario(id);
-    setLoading(false);
-    if (res.errorMessage) {
-      return setErrorMessage(res.errorMessage);
-    }
-    setValues({ ...res[0] });
-  }
 
   useEffect(() => {
+    const getData = async (id) => {
+      setErrorMessage('');
+      setLoading(true);
+      const res = await getUsuario(id);
+      setLoading(false);
+      if (res.errorMessage) {
+        return setErrorMessage(res.errorMessage);
+      }
+      setValues({ ...res[0] });
+    }
     if (id) {
       getData(id);
     }
   }, [id]);
 
+  useEffect(() => {
+    const getProfile = async (id) => {
+      setErrorMessage('');
+      setLoading(true);
+      const res = await getPerfilesUsuario(id);
+      setLoading(false);
+      if (res.errorMessage) {
+        return setErrorMessage(res.errorMessage);
+      }
+      console.log(res)
+      setPerfiles(res);
+    }
+    if (id) {
+      getProfile(id);
+    }
+  }, [id]);
+
+  const onEdit = ({ idPerfil }) => {
+    console.log({ idPerfil, idUsuario: id });
+  }
+
   return (
     <Layout title="Datos usuario">
-      <div>
-        <h1>Datos de usuario</h1>
-        {errorMessage && <ErrorMessage message={errorMessage} />}
-        {loading ? 'Cargando...' :
-          !errorMessage && <>
-            <ListItem title="Nombre" description={values?.Nombre} />
-            <ListItem title="Apellido" description={values?.Apellido} />
-            <ListItem title="Usuario" description={values?.Usuario} />
-          </>
-        }
-      </div>
+      <h1>Datos de usuario</h1>
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+      {loading ? 'Cargando...' :
+        !errorMessage && <>
+          <ListItem title="Nombre" description={values?.Nombre} />
+          <ListItem title="Apellido" description={values?.Apellido} />
+          <ListItem title="Usuario" description={values?.Usuario} />
+        </>
+      }
+      <PerfilesUsuarioList list={perfiles} onEdit={onEdit} readonly />
+      <AppLink
+        href={`/usuarios/edit/${id}`}
+        title='Editar usuario' />
     </Layout>)
 }
 
