c992a1b211589220d198cbb4a714a8e258a6ae60
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-19 22:03:49 -0300

    Perfiles usuarios list on edit user page

--- pages/usuarios/edit/[id]/index.js
+++ pages/usuarios/edit/[id]/index.js
@@ -3,8 +3,9 @@ import Router, { useRouter } from 'next/router';
 
 import Form from '../../../../components/Form/Form.component';
 import Layout from '../../../../components/Layout';
-import { editarUsuario, getUsuario } from '../../../../services/usuarios.service';
+import { editarUsuario, getPerfilesUsuario, getUsuario } from '../../../../services/usuarios.service';
 import ErrorMessage from '../../../../components/ErrorMessage/ErrorMessage.component';
+import PerfilesUsuarioList from '../../../../components/PerfilesUsuarioList/PerfilesUsuarioList.component';
 
 const EditarUsuarioForm = [
   {
@@ -38,28 +39,49 @@ const EditUser = () => {
   const [values, setValues] = useState({});
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
-
-  const getData = async (id) => {
-    setLoading(true);
-    const data = await getUsuario(id);
-    setValues(data ? data[0] : {});
-    setLoading(false);
-  }
+  const [perfiles, setPerfiles] = useState([]);
 
   useEffect(() => {
+    const getData = async (id) => {
+      setLoading(true);
+      const data = await getUsuario(id);
+      setValues(data ? data[0] : {});
+      setLoading(false);
+    }
     if (id) {
       getData(id);
     }
     return () => {
       setErrorMessage('');
     }
-  }, [id])
+  }, [id]);
+
+  useEffect(() => {
+    const getProfile = async (id) => {
+      setErrorMessage('');
+      setLoading(true);
+      const res = await getPerfilesUsuario(id);
+      setLoading(false);
+      if (res.errorMessage) {
+        return setErrorMessage(res.errorMessage);
+      }
+      console.log(res)
+      setPerfiles(res);
+    }
+    if (id) {
+      getProfile(id);
+    }
+  }, [id]);
 
   const onSubmit = async (data) => {
     const { Nombre, Apellido, Usuario } = data;
     const res = await editarUsuario({ id, Nombre, Apellido, Usuario })
     if (res.errorMessage) return setErrorMessage(res.errorMessage);
     Router.push('/usuarios')
+  }
+
+  const onEdit = ({ idPerfil }) => {
+    console.log({ idPerfil, idUsuario: id });
   }
 
   return (
@@ -72,10 +94,11 @@ const EditUser = () => {
           <Form
             onFormSubmit={onSubmit}
             config={EditarUsuarioForm}
-            buttonLabel='Editar'
+            buttonLabel='Guardar'
             defaultValues={{ ...values }} />
         </>
       }
+      <PerfilesUsuarioList list={perfiles} onEdit={onEdit} />
     </Layout>
   )
 }
