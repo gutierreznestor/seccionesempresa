61e707031b4e210b8fc21043ef6ff76373ad86c7
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-20 18:23:22 -0300

    Auditoria page

--- pages/auditoria/index.js
+++ pages/auditoria/index.js
@@ -1,32 +1,12 @@
-import React, { useState, useEffect } from 'react';
-
-import { getLogsUsuarios } from '../../services/logs.service';
-
+import React from 'react';
 import Layout from '../../components/Layout';
-import LogsUsuariosList from '../../components/LogsUsuariosList/LogsUsuariosList.component';
-import ErrorMessage from '../../components/ErrorMessage/ErrorMessage.component';
+import AppLink from '../../components/AppLink/AppLink.component';
 
 const Auditoria = () => {
-  const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [logsUsuarios, setLogsUsuarios] = useState([]);
-  useEffect(() => {
-    const getData = async () => {
-      setLoading(true);
-      const data = await getLogsUsuarios();
-      if (data.errorMessage) return setErrorMessage(data.errorMessage)
-      setLoading(false)
-      setLogsUsuarios(data);
-    }
-    getData();
-  }, []);
   return (
     <Layout title="Auditoría">
-      {errorMessage && <ErrorMessage message={errorMessage} />}
-      {loading ?
-        <span>Cargando...</span> :
-        !errorMessage && <LogsUsuariosList list={logsUsuarios} />
-      }
+      <AppLink href='/auditoria/usuarios' title="Usuarios" />
+      <AppLink href='/auditoria/empleados' title="Empleados" />
     </Layout>
   )
 }
