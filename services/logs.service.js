282a05754e58326c7818e51b4bcc6af3621099d7
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-20 00:45:48 -0300

    get logs usuarios fix import

--- services/logs.service.js
+++ services/logs.service.js
@@ -1,5 +1,5 @@
 export const getLogsUsuarios = async () => {
-  const res = await fetch('/api/logsUsuarios/get-logsUsuarios', {
+  const res = await fetch('/api/logsUsuarios/get-logs-usuarios', {
     method: 'GET',
   });
   return await res.json();
