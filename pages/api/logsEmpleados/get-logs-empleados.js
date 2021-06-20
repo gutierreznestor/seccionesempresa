11eb5c39183ecebaf324313d8737a450e0080b21
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-20 18:26:11 -0300

    get logs empleados api

---
+++ pages/api/logsEmpleados/get-logs-empleados.js
@@ -0,0 +1,18 @@
+import { query } from '../../../lib/db'
+
+const handler = async (_, res) => {
+  try {
+    const results = await query(`
+    SELECT DISTINCT le.idLogUsuario, le.Creado, usuarios.Usuario,
+      usuarios.idUsuario, le.Operacion, le.Descripcion
+    FROM logs_empleados AS le
+    INNER JOIN usuarios ON (usuarios.idUsuario = le.idUsuario)
+    `);
+
+    return res.json(results)
+  } catch (e) {
+    res.status(500).json({ errorMessage: e.message })
+  }
+}
+
+export default handler
