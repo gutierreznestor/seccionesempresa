47f1fbdc1cf54279ded386f51fda6b65b9dc3e43
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-20 21:01:44 -0300

    Add log perfiles api

---
+++ pages/api/logsPerfiles/add-log-perfil.js
@@ -0,0 +1,25 @@
+import { query } from '../../../lib/db';
+
+const handler = async (req, res) => {
+  const { idUsuario, Operacion, Descripcion } = req.body;
+  try {
+    if (!idUsuario?.toString().trim() || !Operacion?.trim() || !Descripcion?.trim()) {
+      return res
+        .status(400)
+        .json({ errorMessage: 'Complete todos los campos.' })
+    }
+    const results = await query(
+      `
+      INSERT INTO logs_perfiles(idUsuario, Operacion, Descripcion) 
+      VALUES (?, ?, ?);
+      `,
+      [idUsuario, Operacion, Descripcion],
+    );
+
+    return res.json(results)
+  } catch (e) {
+    res.status(400).json({ errorMessage: e.message })
+  }
+}
+
+export default handler
