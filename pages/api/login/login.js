7d3cb4205d76b2be7851e5d1e64d356326978999
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-21 00:06:34 -0300

    Return token with idUsuario and Perfiles

--- pages/api/login/login.js
+++ pages/api/login/login.js
@@ -1,6 +1,8 @@
-import { compare } from 'bcrypt'
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
 
 import { query } from '../../../lib/db';
+import { getPerfilesUsuario } from '../../../services/usuarios.service';
 
 const handler = async (req, res) => {
   const { Usuario, Password } = req.body;
@@ -10,33 +12,51 @@ const handler = async (req, res) => {
       .json({ errorMessage: 'Complete todos los campos.' })
   }
 
-
-  const results = await query(
+  const resultsUser = await query(
     `
-    SELECT Usuario, Password FROM usuarios
+    SELECT idUsuario, Usuario, Password 
+    FROM usuarios
     WHERE usuarios.Usuario=?;
     `,
     [Usuario],
   );
 
-  if (!results?.length) {
+  if (!resultsUser?.length) {
     return res.status(401).json({ errorMessage: 'El usuario o la contraseña son inválidas.' });
   };
-  const user = results[0];
-  compare(Password, user.Password, function (err, result) {
-    if (!result) {
-      return res.status(401).json({ errorMessage: 'El usuario o la contraseña son inválidas.' });
-    }
-    return res.status(201).json({ errorMessage: 'Inicio de sesión correcto.' });
-    return res.json({ errorMessage: result })
-    if (!err && result) {
-      const claims = { sub: person.id, myPersonEmail: person.email };
-      const jwt = sign(claims, secret, { expiresIn: '1h' });
-      res.json({ authToken: jwt });
-    } else {
-      res.json({ errorMessage: 'Ups, something went wrong!' });
-    }
-  });
+  try {
+    const user = resultsUser[0];
+    const resultsProfile = await query(`
+      SELECT perfiles.idPerfil, perfiles.Nombre AS Perfil, IF(up.idUsuarioPerfil IS NULL, 'No', 'Sí') AS TienePerfil, up.idUsuario
+      FROM perfiles
+      LEFT JOIN usuarios_tiene_perfiles as up ON 
+        (up.idPerfil = perfiles.idPerfil AND up.idUsuario=?)
+      WHERE up.idUsuario IS NOT NULL
+      GROUP BY perfiles.idPerfil   
+      `,
+      [user.idUsuario],
+    );
+
+    compare(Password, user.Password, function (err, result) {
+      if (!result) {
+        res.status(401).json({ errorMessage: 'El usuario o la contraseña son inválidas.*' });
+      };
+      if (!err && result) {
+        const userPayload = {
+          idUsuario: user.idUsuario,
+          Nombre: user.Nombre,
+          Perfiles: resultsProfile.map((result) => result.Perfil),
+        }
+        const claims = { user: userPayload };
+        const jwt = sign(claims, 'secret', { expiresIn: '7d' });
+        res.json({ authToken: jwt });
+      } else {
+        res.json({ errorMessage: 'Ups, something went wrong!' });
+      };
+    });
+  } catch (error) {
+    res.json({ errorMessage: error.message });
+  }
 }
 
 export default handler
