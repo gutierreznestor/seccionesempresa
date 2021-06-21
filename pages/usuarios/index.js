0d95d743b53f0dbb045d1a03fe477e539fb60b6c
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-21 19:25:08 -0300

    Initial props usuarios

--- pages/usuarios/index.js
+++ pages/usuarios/index.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Router from 'next/router';
+import { verify } from 'jsonwebtoken';
 
 import Layout from '../../components/Layout';
 import AppLink from '../../components/AppLink/AppLink.component';
@@ -7,32 +8,14 @@ import UsuariosList from '../../components/UsuariosList/UsuariosList.component';
 import { deleteUsuario, getUsuarios } from '../../services/usuarios.service';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage.component';
 
-const Usuarios = () => {
-  const [loading, setLoading] = useState(false);
-  const [list, setList] = useState([]);
+const Usuarios = ({ listUsuarios }) => {
   const [errorMessage, setErrorMessage] = useState('');
-
-  const fetchUsuarios = async () => {
-    setLoading(true);
-    const data = await getUsuarios();
-    setLoading(false);
-    if (data.errorMessage) return setErrorMessage(data.errorMessage)
-    setList(data);
-  }
-
-  useEffect(() => {
-    fetchUsuarios();
-    return () => {
-      setErrorMessage('');
-    }
-  }, []);
 
   const onDelete = async (id) => {
     const ok = confirm('¿Quieres eliminar al usuario?');
     if (ok) {
       setLoading(true);
       const data = await deleteUsuario({ idUsuario: 1, id });
-      fetchUsuarios();
       if (data.errorMessage) return setErrorMessage(data.errorMessage);
       Router.push('usuarios');
     }
@@ -43,14 +26,28 @@ const Usuarios = () => {
       <h1>Usuarios</h1>
       {errorMessage && <ErrorMessage message={errorMessage} />}
       <AppLink href='/usuarios/new' title='Nuevo usuario' />
-      {loading ?
-        <span>Cargando...</span> :
-        <UsuariosList
-          list={list}
-          onDelete={onDelete}
-        />}
+      <UsuariosList
+        list={listUsuarios}
+        onDelete={onDelete}
+      />
     </Layout>
   )
+}
+Usuarios.getInitialProps = async (ctx) => {
+  const cookie = ctx.req?.headers.cookie;
+  const resp = await fetch('http://localhost:3000/api/usuarios/get-usuarios', {
+    headers: {
+      cookie,
+    }
+  })
+  const listUsuarios = await resp.json();
+  let user = null;
+  verify(cookie, 'secret', async (err, decoded) => {
+    if (!err && decoded) {
+      user = decoded.user;
+    }
+  });
+  return { listUsuarios, user };
 }
 
 export default Usuarios;
