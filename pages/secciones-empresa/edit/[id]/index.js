5c813ccc315461d06fea34d56e565d0944b0fb3c
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-21 23:29:17 -0300

    User prop on edit seccion

--- pages/secciones-empresa/edit/[id]/index.js
+++ pages/secciones-empresa/edit/[id]/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Router, { useRouter } from 'next/router';
+import { verify } from 'jsonwebtoken';
+import fetch from 'isomorphic-unfetch';
 
 import Form from '../../../../components/Form/Form.component';
 import Layout from '../../../../components/Layout';
-import { editarSeccionEmpresa, getSeccionEmpresa } from '../../../../services/seccionesEmpresa.service';
+import { editarSeccionEmpresa } from '../../../../services/seccionesEmpresa.service';
 import ErrorMessage from '../../../../components/ErrorMessage/ErrorMessage.component';
 
 const EditarSeccionForm = [
@@ -17,28 +19,27 @@ const EditarSeccionForm = [
   },
 ];
 
-const EditarSeccion = () => {
-  const { query: { id } } = useRouter();
-  const [values, setValues] = useState({});
+const EditarSeccion = ({ data, user }) => {
+  const [values, setValues] = useState(data);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const getData = async (id) => {
-    setLoading(true);
-    const data = await getSeccionEmpresa(id);
-    setValues(data ? data[0] : {});
-    setLoading(false);
-  }
+  // const getData = async (id) => {
+  //   setLoading(true);
+  //   // const data = await getSeccionEmpresa(id);
+  //   setValues(data ? data[0] : {});
+  //   setLoading(false);
+  // }
 
-  useEffect(() => {
-    if (id) {
-      getData(id);
-    }
-  }, [id])
+  // useEffect(() => {
+  //   if (id) {
+  //     // getData(id);
+  //   }
+  // }, [id])
 
   const onSubmit = async (data) => {
-    const { Nombre } = data;
-    const res = await editarSeccionEmpresa({ user: 9, id, Nombre })
+    const { id, Nombre } = data;
+    const res = await editarSeccionEmpresa({ user: user?.idUsuario, id, Nombre })
     if (res.errorMessage) {
       setErrorMessage(res.errorMessage);
     } else {
@@ -65,12 +66,21 @@ const EditarSeccion = () => {
 export default EditarSeccion;
 
 EditarSeccion.getInitialProps = async (ctx) => {
-  const cookie = ctx.req?.headers.cookie;
-  const respSE = await fetch(`http://localhost:3000/api/secciones-empresa/get-seccion-empresa?id=${ctx.req?.query.id}`, {
+  const cookie = ctx.req?.cookies.auth;
+  const respSE = await fetch(`http://localhost:3000/api/secciones-empresa/get-seccion-empresa?id=${ctx.query?.id}`, {
     headers: {
       cookie,
     }
   })
-  const listSecciones = await respSE.json();
-  return { listSecciones };
+  let res = await respSE.json();
+  let data = res && res.length ? res[0] : {};
+  data.id = ctx.query?.id;
+  let user = null;
+  verify(cookie, 'secret', async (err, decoded) => {
+    console.log({ decoded });
+    if (!err && decoded) {
+      user = decoded.user;
+    }
+  });
+  return { data, user };
 }
