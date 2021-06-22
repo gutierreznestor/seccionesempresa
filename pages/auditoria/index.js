ac2ab1c6cf28c6378d109bd79e8f02c48af9b2e6
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-22 11:04:07 -0300

    getServerSideProps on auditoria index

--- pages/auditoria/index.js
+++ pages/auditoria/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import AppLink from '../../components/AppLink/AppLink.component';
-import fetch from 'isomorphic-unfetch';
 import { verify } from 'jsonwebtoken';
+import parseCookies from '../../helpers/parseCookies';
 
-const Auditoria = ({ userProfiles }) => {
+const Auditoria = ({ user }) => {
   return (
     <Layout title="Auditoría">
       <AppLink href='/auditoria/seccionesEmpresa' title="Secciones empresa" />
@@ -15,23 +15,17 @@ const Auditoria = ({ userProfiles }) => {
   )
 }
 
-Auditoria.getInitialProps = async (ctx) => {
-  const cookie = ctx.req?.cookies.auth;
-  const respSE = await fetch(`http://localhost:3000/api/secciones-empresa/get-secciones-empresa`, {
-    headers: {
-      cookie,
-    }
-  })
-  let res = await respSE.json();
-  let data = res && res.length ? res[0] : {};
-  data.id = ctx.query?.id;
+export async function getServerSideProps(ctx) {
+  const cookie = parseCookies(ctx.req);
   let user = null;
-  verify(ctx.req?.cookies.auth, 'secret', async (err, decoded) => {
+  verify(cookie.auth, 'secret', async (err, decoded) => {
     if (!err && decoded) {
       user = decoded.user;
     }
   });
-  return { data, user };
-};
+  return {
+    props: { user },
+  }
+}
 
 export default Auditoria;
