01ba98f009f47111e352b12d61defacbdf9bfe88
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-22 11:00:19 -0300

    getInitialProps removed from _app

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,4 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-MyApp.getInitialProps = async ({ Component, ctx }) => {
-  return {
-    pageProps: {
-      ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
-    }
-  }
-}
-
 export default MyApp
