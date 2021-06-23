699cdb617567816362e6ac8a8bc140f056a2890b
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-22 22:03:58 -0300

    getServerSideProps on perfiles page

--- pages/perfiles/index.js
+++ pages/perfiles/index.js
@@ -1,47 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Router from 'next/router';
+import fetch from 'isomorphic-unfetch';
+import { verify } from 'jsonwebtoken';
 
 import Layout from '../../components/Layout'
 import PerfilesList from '../../components/PerfilesList/PerfilesList.component'
 import AppLink from '../../components/AppLink/AppLink.component';
-import { deletePerfiles, getPerfiles } from '../../services/perfiles.service';
+import { deletePerfiles } from '../../services/perfiles.service';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage.component';
+import parseCookies from '../../helpers/parseCookies';
 
-const Perfiles = () => {
-  const [loading, setLoading] = useState(false);
-  const [list, setList] = useState([]);
+const Perfiles = ({ data, user }) => {
   const [errorMessage, setErrorMessage] = useState('');
-
-  const fetchPerfiles = async () => {
-    setLoading(true);
-    const data = await getPerfiles();
-    setLoading(false);
-    if (data.errorMessage) return setErrorMessage(data.errorMessage)
-    setList(data);
-  }
-
-  useEffect(() => {
-    fetchPerfiles();
-    return () => {
-      setErrorMessage('');
-    }
-  }, []);
 
   const onDelete = async (id) => {
     setErrorMessage('');
     const ok = confirm('¿Quieres eliminar el perfil?');
     if (ok) {
-      setLoading(true);
-      const data = await deletePerfiles({ user: 3, id });
-      setLoading(false);
+      const data = await deletePerfiles({ user: user?.idUsuario, id });
       if (data.errorMessage) return setErrorMessage(data.errorMessage);
-      fetchPerfiles();
       Router.push('perfiles');
     }
-  }
-
-  const onEdit = (id) => {
-
   }
 
   return (
@@ -49,14 +28,31 @@ const Perfiles = () => {
       <h1>Perfiles</h1>
       <AppLink href='/perfiles/new' title='Nuevo Perfil' />
       {errorMessage && <ErrorMessage message={errorMessage} />}
-      {loading ?
-        <span>Cargando...</span> :
-        <PerfilesList
-          list={list}
-          onDelete={onDelete}
-          onEdit={onEdit} />}
+      <PerfilesList
+        list={data}
+        onDelete={onDelete} />
     </Layout>
   )
+}
+
+export async function getServerSideProps(ctx) {
+  const cookie = parseCookies(ctx.req);
+  const respSE = await fetch('http://localhost:3000/api/perfiles/get-perfiles', {
+    headers: {
+      cookie,
+    }
+  })
+  let res = await respSE.json();
+  let data = (res && res.length) ? res : [];
+  let user = null;
+  verify(cookie.auth, 'secret', async (err, decoded) => {
+    if (!err && decoded) {
+      user = decoded.user;
+    }
+  });
+  return {
+    props: { data, user },
+  }
 }
 
 export default Perfiles;
