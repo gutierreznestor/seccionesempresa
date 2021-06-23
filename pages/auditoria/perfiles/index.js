f8c5f04c3bbbc38d6694020e538e48eb501ffacc
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-23 20:31:55 -0300

    Error prop on auditoria perfil page

--- pages/auditoria/perfiles/index.js
+++ pages/auditoria/perfiles/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import fetch from 'isomorphic-unfetch';
 import { verify } from 'jsonwebtoken';
 
@@ -8,14 +8,12 @@ import ErrorMessage from '../../../components/ErrorMessage/ErrorMessage.componen
 import parseCookies from '../../../helpers/parseCookies';
 import { redirectToLogin } from '../../../helpers/redirectToLogin';
 
-const AuditoriaPerfiles = ({ data, user }) => {
-  const [errorMessage, setErrorMessage] = useState('');
-
+const AuditoriaPerfiles = ({ data, user, error }) => {
   return (
     <Layout title="Auditoría Perfiles" user={user}>
-      {errorMessage && <ErrorMessage message={errorMessage} />}
+      {error && <ErrorMessage message={error} />}
       {
-        !errorMessage && <LogsPerfilesList list={data} />
+        !error && <LogsPerfilesList list={data} />
       }
     </Layout>
   )
@@ -26,21 +24,25 @@ export async function getServerSideProps(ctx) {
   if (!cookie.auth) {
     redirectToLogin(ctx.res);
   }
-  const respSE = await fetch(`http://localhost:3000/api/logsPerfiles/get-logs-perfiles`, {
+  const res = await fetch(`http://localhost:3000/api/logsPerfiles/get-logs-perfiles`, {
     headers: {
       cookie,
     }
   })
-  let res = await respSE.json();
-  let data = (res && res.length) ? res : [];
   let user = null;
   verify(cookie.auth, 'secret', async (err, decoded) => {
     if (!err && decoded) {
       user = decoded.user;
     }
   });
+  let data = await res.json();
+  let error = null;
+  if (data.errorMessage) {
+    error = data.errorMessage;
+    data = [];
+  }
   return {
-    props: { data, user },
+    props: { data, user, error },
   }
 }
 
