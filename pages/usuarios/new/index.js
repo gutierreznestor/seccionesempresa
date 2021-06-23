ad77d28c24336ea358f567c45fd62aeca49f9b3a
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-23 20:09:33 -0300

    Error prop on new user

--- pages/usuarios/new/index.js
+++ pages/usuarios/new/index.js
@@ -44,8 +44,8 @@ const NuevoUsuarioForm = [
   },
 ];
 
-const NuevoUsuario = ({ user }) => {
-  const [errorMessage, setErrorMessage] = useState('');
+const NuevoUsuario = ({ user, error }) => {
+  const [errorMessage, setErrorMessage] = useState(error);
 
   useEffect(() => {
     return () => {
@@ -74,14 +74,25 @@ export async function getServerSideProps(ctx) {
   if (!cookie.auth) {
     redirectToLogin(ctx.res);
   }
+  const resp = await fetch('http://localhost:3000/api/usuarios/get-usuarios', {
+    headers: {
+      cookie,
+    }
+  });
   let user = null;
   verify(cookie.auth, 'secret', async (err, decoded) => {
     if (!err && decoded) {
       user = decoded.user;
     }
   });
+  let data = await resp.json();
+  let error = null;
+  if (data.errorMessage) {
+    error = data.errorMessage;
+    data = [];
+  }
   return {
-    props: { user },
+    props: { data, user, error },
   }
 }
 
