58563662b4017d80f25209d5b28aaadbeeeedcbe
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-23 01:04:51 -0300

    No ling component

--- components/Navbar/Navbar.component.js
+++ components/Navbar/Navbar.component.js
@@ -1,38 +1,52 @@
 import React from 'react';
 import Link from 'next/link';
 
+import { isAllowed } from '../../hocs/auth';
 import { StyledNavbar } from './Navbar.styled';
+import NoLink from '../NoLink/NoLink.component';
 
-const links = [
-  {
-    label: 'Secciones empresa',
-    url: '/secciones-empresa',
-  },
-  {
-    label: 'Empleados',
-    url: '/empleados',
-  },
-  {
-    label: 'Usuarios',
-    url: '/usuarios',
-  },
-  {
-    label: 'Perfiles',
-    url: '/perfiles',
-  },
-  {
-    label: 'Auditoría',
-    url: '/auditoria',
-  },
-];
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
+
+  console.log({ Navbaruser: user });
+
+  const links = [
+    {
+      label: 'Secciones empresa',
+      url: '/secciones-empresa',
+    },
+    {
+      label: 'Empleados',
+      url: '/empleados',
+    },
+    {
+      label: 'Usuarios',
+      url: '/usuarios',
+    },
+    {
+      label: 'Perfiles',
+      url: '/perfiles',
+    },
+    {
+      label: 'Auditoría',
+      url: '/auditoria',
+      disabled: isAllowed(['auditor'], user.Perfiles)
+    },
+    {
+      label: 'Cerrar sesión',
+      url: '/logout',
+    },
+  ];
+
   return (
     <StyledNavbar>
       <ul>
-        {links.map(({ label, url }) => (
-          <li key={url}><Link href={url}><a>{label}</a></Link></li>
-        ))}
+        {links.map(({ label, url, disabled }) => {
+          const link = disabled ?
+            <NoLink label={label} /> :
+            <Link href={url}><a>{label}</a></Link>;
+          return <li key={url}>{link}</li>
+        })}
       </ul>
     </StyledNavbar>
   )
---
+++ components/NoLink/NoLink.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoLink = ({ label }) => {
+  return (
+    <span style={{ color: '#949494' }}>{label}</span>
+  )
+}
+
+export default NoLink;
