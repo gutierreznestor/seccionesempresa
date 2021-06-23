3482a449679fb9d47fb08cb4cfe4f6fc74a942a7
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-23 00:16:05 -0300

    Remove index page

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import fetch from 'isomorphic-unfetch';
-import { verify } from 'jsonwebtoken';
+import React from 'react';
+import Router from 'next/router';
 
-import ErrorMessage from '../components/ErrorMessage/ErrorMessage.component';
-import Layout from '../components/Layout'
-import SeccionesEmpresaList from '../components/SeccionesEmpresaList/SeccionesEmpresaList.component'
-import EmpleadosList from '../components/EmpleadosList/EmpleadosList.component';
-import parseCookies from '../helpers/parseCookies';
-import { redirectToLogin } from '../helpers/redirectToLogin';
-
-const Home = ({ listSecciones, listEmpleados, user }) => {
-  const [errorMessage, setErrorMessage] = useState('');
-  useEffect(() => {
-    return () => {
-      setErrorMessage('');
-    }
-  }, []);
-
-  return (
-    <Layout title="Home">
-      {errorMessage && <ErrorMessage message={errorMessage} />}
-      <h2>Secciones empresa</h2>
-      {
-        !errorMessage && <SeccionesEmpresaList list={listSecciones} readonly />
-      }
-      <h2>Empleados</h2>
-      {
-        !errorMessage && <EmpleadosList list={listEmpleados} readonly />
-      }
-    </Layout>
-  )
+const index = () => {
+  React.useEffect(() => {
+    Router.push('/secciones-empresa');
+  }, [])
+  return null
 }
 
-export async function getServerSideProps(ctx) {
-  const cookie = parseCookies(ctx.req);
-  if (!cookie.auth) {
-    redirectToLogin(ctx.res);
-  }
-  const respSE = await fetch('http://localhost:3000/api/secciones-empresa/get-secciones-empresa', {
-    headers: {
-      cookie,
-    }
-  });
-  const listSecciones = await respSE.json();
-  const respE = await fetch('http://localhost:3000/api/empleados/get-empleados', {
-    headers: {
-      cookie,
-    }
-  });
-  const listEmpleados = await respE.json();
-  let user = null;
-  verify(cookie.auth, 'secret', async (err, decoded) => {
-    if (!err && decoded) {
-      user = decoded.user;
-    }
-  });
-  return {
-    props: { listSecciones, listEmpleados, user }
-  };
-}
-
-export default Home;
+export default index
