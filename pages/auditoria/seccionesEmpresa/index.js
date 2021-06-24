ae4f774a5267c79be44ead0ef8982a0b81a297e0
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-23 22:43:27 -0300

    Data table on seccion empresa auditoria

--- pages/auditoria/seccionesEmpresa/index.js
+++ pages/auditoria/seccionesEmpresa/index.js
@@ -3,18 +3,45 @@ import fetch from 'isomorphic-unfetch';
 import { verify } from 'jsonwebtoken';
 
 import Layout from '../../../components/Layout';
-import LogsSeccionesEmpresaList from '../../../components/LogsSeccionesEmpresaList/LogsSeccionesEmpresaList.component';
 import ErrorMessage from '../../../components/ErrorMessage/ErrorMessage.component';
 import parseCookies from '../../../helpers/parseCookies';
 import { redirectToLogin } from '../../../helpers/redirectToLogin';
+import DataTable from '../../../components/DataTable/DataTable.component';
+import SearchInput from '../../../components/SearchInput/SearchInput.component';
 
 const AuditoriaSeccionesEmpresa = ({ data, user, error }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (value) => {
+    setQuery(value)
+  };
+
+  const search = (row) => {
+    return row.filter(row =>
+      row.Creado?.toString().toLowerCase().indexOf(query) > -1 ||
+      row.Usuario?.toString().toLowerCase().indexOf(query) > -1 ||
+      row.idUsuario?.toString().toLowerCase().indexOf(query) > -1 ||
+      row.Operacion?.toString().toLowerCase().indexOf(query) > -1 ||
+      row.Descripcion?.toString().toLowerCase().indexOf(query) > -1
+    );
+  }
 
   return (
     <Layout title="Auditoría Secciones empresa" user={user}>
       {error && <ErrorMessage message={error} />}
       {
-        !error && <LogsSeccionesEmpresaList list={data} />
+        !error &&
+        <>
+          <SearchInput
+            value={query}
+            onChange={handleChange}
+            placeholder="Buscar" />
+          <DataTable
+            data={search(data)}
+            user={user}
+            notAllowed={['auditor']}
+            readonly />
+        </>
       }
     </Layout>
   )
