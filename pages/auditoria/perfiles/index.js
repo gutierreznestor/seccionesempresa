5d0383c329a7d5d7de1de59c712f7b9b00bb02c5
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-24 00:21:08 -0300

    Data table on auditoria perfiles

--- pages/auditoria/perfiles/index.js
+++ pages/auditoria/perfiles/index.js
@@ -2,18 +2,48 @@ import React from 'react';
 import fetch from 'isomorphic-unfetch';
 import { verify } from 'jsonwebtoken';
 
-import Layout from '../../../components/Layout';
-import LogsPerfilesList from '../../../components/LogsPerfilesList/LogsPerfilesList.component';
-import ErrorMessage from '../../../components/ErrorMessage/ErrorMessage.component';
-import parseCookies from '../../../helpers/parseCookies';
 import { redirectToLogin } from '../../../helpers/redirectToLogin';
+import parseCookies from '../../../helpers/parseCookies';
+import Layout from '../../../components/Layout';
+import ErrorMessage from '../../../components/ErrorMessage/ErrorMessage.component';
+import SearchInput from '../../../components/SearchInput/SearchInput.component';
+import DataTable from '../../../components/DataTable/DataTable.component';
 
 const AuditoriaPerfiles = ({ data, user, error }) => {
+
+  const [query, setQuery] = React.useState('');
+
+  const handleChange = (value) => {
+    setQuery(value)
+  };
+
+  const search = (row) => {
+    return row.filter(row =>
+      row.idLogPerfil?.toString().toLowerCase().indexOf(query) > -1 ||
+      row.Creado?.toString().toLowerCase().indexOf(query) > -1 ||
+      row.Usuario?.toString().toLowerCase().indexOf(query) > -1 ||
+      row.idUsuario?.toString().toLowerCase().indexOf(query) > -1 ||
+      row.Operacion?.toString().toLowerCase().indexOf(query) > -1 ||
+      row.Descripcion?.toString().toLowerCase().indexOf(query) > -1
+    );
+  }
+
   return (
     <Layout title="Auditoría Perfiles" user={user}>
       {error && <ErrorMessage message={error} />}
       {
-        !error && <LogsPerfilesList list={data} />
+        !error &&
+        <>
+          <SearchInput
+            value={query}
+            onChange={handleChange}
+            placeholder="Buscar" />
+          <DataTable
+            data={search(data)}
+            user={user}
+            notAllowed={['auditor']}
+            readonly />
+        </>
       }
     </Layout>
   )
