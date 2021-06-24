976d8df9f434fe20204a302d6039916f09a6fb75
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-06-23 22:10:37 -0300

    Search input component

---
+++ components/SearchInput/SearchInput.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledSearchInput = styled.input`
+  padding: 5px;
+  margin: 5px 0;
+  border-radius: 3px;
+  border: none;
+  min-width: 300px;
+`;
