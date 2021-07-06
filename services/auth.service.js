4fc1fd472ad55092214cfb925226d22ee08e99c7
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-07-05 22:56:58 -0300

    Set empresa service

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,6 +1,7 @@
 import fetch from 'isomorphic-unfetch';
 
 export const login = async ({ Usuario, Password }) => {
+  const database = sessionStorage.get('db') || 'empresa';
   const url = `http://localhost:3000/api/login/login`;
   let res = await fetch(url, {
     method: 'POST',
@@ -10,6 +11,7 @@ export const login = async ({ Usuario, Password }) => {
     body: JSON.stringify({
       Usuario,
       Password,
+      database,
     }),
   });
   return await res.json();
@@ -18,5 +20,17 @@ export const login = async ({ Usuario, Password }) => {
 export const logout = async () => {
   return await fetch('http://localhost:3000/api/logout/logout', {
     method: 'GET',
-  })
+  });
+}
+
+export const setEmpresa = async (db) => {
+  return await fetch('http://localhost:3000/api/login/set-db', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      db,
+    }),
+  });
 }
