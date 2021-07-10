1538368715a40163835dcea1871a1e9dfa055286
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-07-10 18:10:22 -0300

    Leer archivos service

--- services/copiasSeguridad.service.js
+++ services/copiasSeguridad.service.js
@@ -12,4 +12,12 @@ export const MakeCopiaSeguridad = async ({ db }) => {
     }),
   });
   return await res.json();
+};
+
+export const LeerArchivos = async ({ db }) => {
+  const url = `http://localhost:3000/api/copiasSeguridad/leer-archivos?db=${db}`;
+  const res = await fetch(url, {
+    method: 'GET',
+  });
+  return await res.json();
 }
