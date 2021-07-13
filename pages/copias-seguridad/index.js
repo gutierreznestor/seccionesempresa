7d7aace1f04d91ef9bc79947286fc22a3ee5dd4a
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-07-13 00:01:53 -0300

    Copias de seguridad page

--- pages/copias-seguridad/index.js
+++ pages/copias-seguridad/index.js
@@ -5,33 +5,47 @@ import { redirectToLogin } from '../../helpers/redirectToLogin';
 
 import Layout from '../../components/Layout';
 import Button from '../../components/Button/Button.component';
-import { LeerArchivos, MakeCopiaSeguridad } from '../../services/copiasSeguridad.service';
+import { leerArchivos, makeCopiaSeguridad, restaurarCopiaSeguridad } from '../../services/copiasSeguridad.service';
 import BackupList from '../../components/BackupList/BackupList.component';
+import ErrorMessage from '../../components/ErrorMessage/ErrorMessage.component';
 
 const CopiasSeguridad = ({ user, db }) => {
 
   const [backups, setBackups] = React.useState([]);
+  const [errorMessage, setErrorMessage] = React.useState('');
 
   const getNames = async () => {
-    const res = await LeerArchivos({ db });
+    const res = await leerArchivos({ db });
     setBackups(res);
   }
 
   const backup = async () => {
-    await MakeCopiaSeguridad({ db });
+    const res = await makeCopiaSeguridad({ db });
+    if (res.errorMessage) {
+      return setErrorMessage(res.errorMessage);
+    }
     getNames()
   }
 
   React.useEffect(() => {
     getNames();
-  }, [getNames, db]);
+  }, [db]);
+
+  const onRestoreBackup = async (value) => {
+    const res = await restaurarCopiaSeguridad({ db, fileName: value });
+    if (res.errorMessage) {
+      return setErrorMessage(res.errorMessage);
+    }
+    console.log({ res });
+  }
 
   return (
     <Layout title="Copias de seguridad" user={user}>
       <h1>Copias de seguridad</h1>
       <h2>{db}</h2>
+      {errorMessage && <ErrorMessage message={errorMessage} />}
       <Button label="Realizar backup" onClick={backup} />
-      {backups.length && <BackupList list={backups} user={user} />}
+      {backups.length && <BackupList list={backups} user={user} onRestoreBackup={onRestoreBackup} />}
     </Layout>
   )
 }
