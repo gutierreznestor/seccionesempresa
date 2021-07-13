04c5e71668494fa769301eabf1489263834915b4
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-07-13 20:53:48 -0300

    Restaurar copia de seguridad service

--- services/copiasSeguridad.service.js
+++ services/copiasSeguridad.service.js
@@ -1,6 +1,6 @@
 import fetch from 'isomorphic-unfetch';
 
-export const MakeCopiaSeguridad = async ({ db }) => {
+export const makeCopiaSeguridad = async ({ db }) => {
   const url = `http://localhost:3000/api/copiasSeguridad/make-copia-seguridad`;
   const res = await fetch(url, {
     method: 'POST',
@@ -14,10 +14,25 @@ export const MakeCopiaSeguridad = async ({ db }) => {
   return await res.json();
 };
 
-export const LeerArchivos = async ({ db }) => {
+export const leerArchivos = async ({ db }) => {
   const url = `http://localhost:3000/api/copiasSeguridad/leer-archivos?db=${db}`;
   const res = await fetch(url, {
     method: 'GET',
+  });
+  return await res.json();
+}
+
+export const restaurarCopiaSeguridad = async ({ db, fileName }) => {
+  const url = `http://localhost:3000/api/copiasSeguridad/restaurar-copia-seguridad`;
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      db,
+      fileName,
+    }),
   });
   return await res.json();
 }
