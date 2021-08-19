86a88b0023ce4d06e43bf47adb63052f0b737c11
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-08-18 21:22:47 -0300

    Nueva empresa api

---
+++ pages/api/empresas/nueva-empresa.js
@@ -0,0 +1,23 @@
+var MysqlTools = require('mysql-tools');
+var tool = new MysqlTools();
+
+import { query } from '../../../lib/db';
+
+const handler = async (req, res) => {
+  try {
+    const { empresa, DB } = req.body;
+    const result = await query(
+      `
+      INSERT INTO empresas(Nombre, DB) 
+      VALUES (?, ?);
+      `,
+      [empresa, DB],
+      'mainseccionesempresa',
+    );
+    return await res.status(201).json(result)
+  } catch (error) {
+    return await res.status(400).json({ errorMessage: error.message })
+  }
+};
+
+export default handler;
