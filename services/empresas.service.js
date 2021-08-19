91e49b8fc4284c8d276ea4e11de97e5c5ac2758d
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-08-19 01:43:07 -0300

    add data after create empresa

--- services/empresas.service.js
+++ services/empresas.service.js
@@ -40,6 +40,16 @@ export const nuevaEmpresa = async ({ empresa, DB }) => {
       DB,
     }),
   });
+  url = `http://localhost:3000/api/empresas/data`;
+  res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      DB,
+    }),
+  });
   return await res.json();
 };
 
