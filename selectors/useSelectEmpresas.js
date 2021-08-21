1e8f0795d8f0a31fcb135c20a771d1c4d1d324ce
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-08-21 20:25:14 -0300

    empresasDropdown in useSelectEmpresas

--- selectors/useSelectEmpresas.js
+++ selectors/useSelectEmpresas.js
@@ -1,9 +1,18 @@
 import { useSelector } from "react-redux";
 
+const apiToDropdown = (list = []) => {
+  const toDropdown = list.map(item => ({
+    label: item.Empresa,
+    value: item.DB,
+  }));
+  return toDropdown;
+}
+
 export const useSelectEmpresas = () => {
   const { empresas } = useSelector(state => state);
   return {
     empresas: empresas?.list,
+    empresasDropdown: apiToDropdown(empresas?.list),
     loading: empresas?.loading,
   };
 }
