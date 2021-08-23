ee741b4e009d0c63eadacefcf2b0fca85bead96b
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-08-22 22:33:53 -0300

    setEmpresa added to rootSaga

--- sideeffects/empresas/index.js
+++ sideeffects/empresas/index.js
@@ -2,11 +2,13 @@ import { all, fork } from 'redux-saga/effects';
 
 import getEmpresas from './get';
 import newEmpresa from './new';
+import setEmpresa from './setEmpresa';
 
 function* rootSaga() {
   yield all([
     fork(getEmpresas),
     fork(newEmpresa),
+    fork(setEmpresa),
   ]);
 }
 
