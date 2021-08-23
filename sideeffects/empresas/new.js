c2469400405cfb239d1bc0013b16301e58c61077
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-08-22 22:34:21 -0300

    new empresa saga

--- sideeffects/empresas/new.js
+++ sideeffects/empresas/new.js
@@ -1,29 +1,72 @@
-import { all, put, takeLatest } from 'redux-saga/effects';
-import { newEmpresa, newEmpresaSuccess, newEmpresaError } from '../../store/empresas';
+import { put, takeLatest, fork } from 'redux-saga/effects';
+import { newEmpresa, newEmpresaSuccess, newEmpresaError, getEmpresas } from '../../store/empresas';
 
+function* crearEmpresa({ empresa, DB }) {
+  const url = `http://localhost:3000/api/empresas/nueva-empresa`;
+  const res = yield fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      empresa,
+      DB,
+    }),
+  });
+  const data = yield res.json()
+  return data;
+}
 
-function* create({ payload }) {
+function* crearBase({ DB }) {
+  const url = `http://localhost:3000/api/empresas/nueva-base`;
+  const res = yield fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      DB,
+    }),
+  });
+  const data = yield res.json()
+  return data;
+}
+
+function* setDb({ DB }) {
+  const url = `http://localhost:3000/api/login/set-db`;
+  const res = yield fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      db: DB,
+    }),
+  });
+  const data = yield res.json()
+  if (data.errorMessage) {
+    return yield put(newEmpresaError(data.errorMessage))
+  }
+}
+
+function* create({ payload: { empresa, DB } }) {
   try {
-    yield console.log('********** payload: ', payload);
-    let url = `http://localhost:3000/api/empresas/nueva-empresa`;
-    let res = yield fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        empresa: payload.empresa,
-        DB: payload.DB,
-      }),
-    });
-    const data = yield res.json()
+    let data = yield fork(crearEmpresa, { empresa, DB });
     if (data.errorMessage) {
-      yield put(newEmpresaError(data.errorMessage))
-    } else {
-      yield put(newEmpresaSuccess(data))
+      return yield put(newEmpresaError(data.errorMessage))
     }
-  } catch (err) {
-    yield put(newEmpresaError(err))
+    data = yield fork(crearBase, { DB });
+    if (data.errorMessage) {
+      return yield put(newEmpresaError(data.errorMessage))
+    }
+    data = yield fork(setDb, { DB });
+    if (data.errorMessage) {
+      return yield put(newEmpresaError(data.errorMessage))
+    }
+    yield put(newEmpresaSuccess("Empresa creada correctamente."));
+    yield put(getEmpresas());
+  } catch (error) {
+    yield put(newEmpresaError(error))
   }
 };
 
