1d0c268aa8b6180f2f837fe7473c00136f7f0c98
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-08-24 19:19:24 -0300

    ConnectedRouter in App component

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import { ConnectedRouter } from 'connected-next-router'
 import { Wrapper } from '../store/store';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ConnectedRouter>
+      <Component {...pageProps} />
+    </ConnectedRouter>
+  );
 }
 
 
