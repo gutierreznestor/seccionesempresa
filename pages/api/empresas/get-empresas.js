2f6fd25c92516c3b21c3c6ec7a10e8e4f655eac8
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-08-25 23:30:59 -0300

    mainQuery

--- pages/api/empresas/get-empresas.js
+++ pages/api/empresas/get-empresas.js
@@ -1,11 +1,11 @@
-import { query } from '../../../lib/db'
+import { mainQuery } from '../../../lib/mainDb';
 
 const handler = async (_, res) => {
   try {
-    const results = await query(`
+    const results = await mainQuery(`
       SELECT DISTINCT DB, Nombre AS Empresa
       FROM empresas;
-    `, [], 'mainSeccionesEmpresa');
+    `, null);
     res.status(200).json(results);
   } catch (e) {
     res.status(500).json({ errorMessage: e.message })
