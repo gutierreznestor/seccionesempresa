f8af2207bce38e1eaa50ec230b3583541ec7ea5d
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-08-26 19:14:38 -0300

    Error response on set-db

--- pages/api/empresas/set-db.js
+++ pages/api/empresas/set-db.js
@@ -2,6 +2,11 @@ import cookies from '../../../helpers/cookies';
 
 const handler = async (req, res) => {
   const { db } = req.body;
+  if (!db) {
+    return res.status(400).json({
+      errorMessage: 'No se envió la base de datos',
+    });
+  }
   res.cookie('db', db);
   res.status(200).json({
     message: 'Set db ok',
