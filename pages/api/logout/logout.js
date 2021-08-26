ca9fac1d47ab8627c97fbc2baca4748080d379f1
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-08-26 19:53:27 -0300

    Doesn't remove db after logout

--- pages/api/logout/logout.js
+++ pages/api/logout/logout.js
@@ -8,15 +8,6 @@ const handler = async (req, res) => {
     maxAge: -1,
     path: '/',
   }));
-
-  res.setHeader('Set-Cookie', cookie.serialize('db', '', {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'strict',
-    maxAge: -1,
-    path: '/',
-  }));
-
   res.end();
 };
 
