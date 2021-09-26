bc2deb9a108bd6b46857db1cddb80e1dccb1d5ac
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-09-26 01:01:26 -0300

    List component

---
+++ components/List/List.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  overflow-y: scroll;
+`;
