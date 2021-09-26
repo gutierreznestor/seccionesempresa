d8cb87e06c33da09aa11c025fed3a624f3824294
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-09-26 01:01:50 -0300

    Helper cuenta

---
+++ components/HelperCuenta/HelperCuenta.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HelperCuentaDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: -150px;
+`;
