d80c6a61a2e35262605407b5334ae91ed1efe993
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-09-29 00:57:54 -0300

    isSameDate helper

--- helpers/dates.js
+++ helpers/dates.js
@@ -1,6 +1,11 @@
-import { format } from 'date-fns'
+import { format, isSameDay } from 'date-fns'
 
 export const formatDate = ({ date, formatString = "dd/MM/yyyy" }) => {
   if (!date) return '';
   return format(new Date(date), formatString)
+};
+
+export const isSameDate = (date1, date2) => {
+  if (!date1 || !date2) return false;
+  return isSameDay(new Date(date1), new Date(date2))
 };
