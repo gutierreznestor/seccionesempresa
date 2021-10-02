8f1d56be9a88de33fbffcbd871bb50365cbad08b
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-10-02 20:30:59 -0300

    AsientosByNumero component with register button

--- components/AsientosByNumero/AsientosByNumero.component.js
+++ components/AsientosByNumero/AsientosByNumero.component.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import useAsientos from '../../customHooks/useAsientos';
+import Button from '../Button/Button.component';
 import DataTable from '../DataTable/DataTable.component';
 import ListItem from '../ListItem';
+import { AsientosByNumeroContainer } from './AsientosByNumero.styled';
 
 const Asiento = ({ db }) => {
   const {
     data: { asientosNumero, diferencia },
   } = useAsientos({ db });
 
+  const onRegister = () => {
+    console.log('register');
+  }
+
   return (
     <div>
       <DataTable data={asientosNumero} />
-      <ListItem title="Total Debe" description={diferencia?.Debe} />
-      <ListItem title="Total Haber" description={diferencia?.Haber} />
-      <ListItem title="Diferencia" description={diferencia?.Diferencia} />
+      <AsientosByNumeroContainer>
+        <div>
+          <ListItem title="Total Debe" description={diferencia?.Debe} />
+          <ListItem title="Total Haber" description={diferencia?.Haber} />
+          <ListItem title="Diferencia" description={diferencia?.Diferencia} />
+        </div>
+        <div>
+          <Button
+            label="Registrar"
+            disabled={diferencia != 1}
+            onClick={onRegister}
+          />
+        </div>
+      </AsientosByNumeroContainer>
     </div>
   )
 }
