7bdb576f2c1ffc30f0c24f8bbd66df1b14395b68
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-10-01 21:16:37 -0300

    getNextAsientoRef with fecha, leyenda y tipoAsiento

--- helpers/getNextAsientoRef.js
+++ helpers/getNextAsientoRef.js
@@ -1,7 +1,9 @@
-export const getNextAsientoRef = ({ Numero, Renglon }) => {
+import { formatDate } from '../helpers/dates';
+
+export const getNextAsientoRef = ({ Fecha, Leyenda, Numero, Renglon, TipoAsiento }) => {
   let nuevoAsientoRef = `/contabilidad/asientos/new`;
   if (Numero?.length && Renglon?.length) {
-    nuevoAsientoRef = nuevoAsientoRef + `?Numero=${new Number(Numero)}&Renglon=${new Number(Renglon) + 1}`;
+    nuevoAsientoRef = nuevoAsientoRef + `?Numero=${new Number(Numero)}&Renglon=${new Number(Renglon) + 1}&Fecha=${formatDate({ date: Fecha })}&Leyenda=${Leyenda}&TipoAsiento=${TipoAsiento}`;
   }
   return nuevoAsientoRef;
 }
