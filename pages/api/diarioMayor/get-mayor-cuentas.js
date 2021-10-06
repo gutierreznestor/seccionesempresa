c0dab9b043189b95d1116d4798f57ae553786ec8
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-10-06 00:41:47 -0300

    get-mayor-cuentas using helpers

--- pages/api/diarioMayor/get-mayor-cuentas.js
+++ pages/api/diarioMayor/get-mayor-cuentas.js
@@ -1,37 +1,24 @@
 import { query } from '../../../lib/db'
+import getCuentas from './helper/getCuentas';
+import getMayorCuenta from './helper/getMayorCuenta';
 
 const handler = async (req, res) => {
   try {
-    const { db, Fecha } = req.query;
+    const { db, FechaHasta } = req.query;
     let whereClouse = '';
-    console.log({ db, Fecha });
-    if (Fecha) {
-      whereClouse = `WHERE Fecha <= '${Fecha}'`;
+    if (FechaHasta) {
+      whereClouse = `WHERE Fecha <= '${FechaHasta}'`;
     }
-    //whereClouse += ` AND diario_mayor.idPlanCuenta=4`
-    const queryString = `
-      SELECT DISTINCT 
-        plan_cuentas.Nombre AS Plan, 
-        DATE_FORMAT(diario_mayor.Fecha, '%d-%m-%Y') AS Fecha, 
-        diario_mayor.Numero, 
-        diario_mayor.Renglon,
-        diario_mayor.Leyenda,
-        IF (diario_mayor.DebeHaber = 0, diario_mayor.importe, '') AS Deb,
-        IF (diario_mayor.DebeHaber = 1, diario_mayor.importe, '') AS Cred
-      FROM diario_mayor
-      INNER JOIN plan_cuentas ON diario_mayor.idPlanCuenta = plan_cuentas.idPlanCuenta
-      ${whereClouse}
-      ORDER BY diario_mayor.idPlanCuenta ASC,
-        diario_mayor.Fecha,
-        diario_mayor.TipoAsiento,
-        diario_mayor.Numero,
-        diario_mayor.DebeHaber,
-        diario_mayor.Renglon
-    `;
-    console.log('queryString: ', queryString);
-    const results = await query(queryString, null, db);
-
-    return res.status(200).json(results)
+    const cuentas = await getCuentas({ db });
+    const mayorCuentasPromises = [];
+    if (cuentas.length) {
+      cuentas.forEach(cuenta => {
+        mayorCuentasPromises.push(getMayorCuenta({ idPlanCuenta: cuenta.idPlanCuenta, db, FechaHasta }));
+      });
+    }
+    const mayorCuentas = await Promise.all(mayorCuentasPromises);
+    console.log('mayorCuentas: ', mayorCuentas);
+    return res.status(200).json(mayorCuentas)
   } catch (e) {
     res.status(500).json({ errorMessage: e.message })
   }
