e0e8d4392fe7b3c311515027cc41963c0d5f50d5
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-10-08 21:29:38 -0300

    format and previous date with timezone

--- helpers/dates.js
+++ helpers/dates.js
@@ -1,11 +1,24 @@
-import { format, isSameDay } from 'date-fns'
+import { format, isSameDay, subDays } from 'date-fns';
+import { toDate, zonedTimeToUtc } from 'date-fns-tz';
+const timeZone = 'America/Argentina/Buenos_Aires';
+
 
 export const formatDate = ({ date, formatString = "dd/MM/yyyy" }) => {
-  if (!date) return format(new Date(), formatString);
-  return format(new Date(date), formatString)
+  if (!date) return null;
+  const date2 = toDate(date);
+  const timeInBuenosAires = zonedTimeToUtc(date2, timeZone);
+  const formated = format(timeInBuenosAires, formatString);
+  return formated;
 };
 
 export const isSameDate = (date1, date2) => {
   if (!date1 || !date2) return false;
-  return isSameDay(new Date(date1), new Date(date2))
+  return isSameDay(new Date(date1), new Date(date2));
+};
+
+export const getPreviousDate = (date) => {
+  if (!date) return null;
+  const date2 = toDate(date);
+  const timeInBuenosAires = zonedTimeToUtc(date2, timeZone);
+  return subDays(timeInBuenosAires, 1);
 };
