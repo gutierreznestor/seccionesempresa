fb45e2049f51fbdbb2f94781c33c538604594293
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-10-11 12:51:12 -0300

    refactor getNextAsientoRef

--- helpers/getNextAsientoRef.js
+++ helpers/getNextAsientoRef.js
@@ -1,9 +1,18 @@
 import { formatDate } from '../helpers/dates';
 
-export const getNextAsientoRef = ({ Fecha, Leyenda, Numero, Renglon, TipoAsiento }) => {
-  let nuevoAsientoRef = `/contabilidad/asientos/new`;
-  if (Numero?.length && Renglon?.length) {
-    nuevoAsientoRef = nuevoAsientoRef + `?Numero=${new Number(Numero)}&Renglon=${new Number(Renglon) + 1}&Fecha=${formatDate({ date: Fecha, formatString: 'MM/dd/yyyy', })}&Leyenda=${Leyenda}&TipoAsiento=${TipoAsiento}`;
+export const getNextAsientoRef = ({
+  Fecha,
+  Leyenda,
+  Numero,
+  Renglon,
+  TipoAsiento,
+  path = 'new',
+}) => {
+  let nuevoAsientoRef = `/contabilidad/asientos/${path}`;
+  if (Numero && Renglon) {
+    nuevoAsientoRef = nuevoAsientoRef + `?Numero=${new Number(Numero)}&Renglon=${new Number(Renglon) + 1}`;
+    nuevoAsientoRef = nuevoAsientoRef + `&Fecha=${formatDate({ date: Fecha, formatString: 'MM/dd/yyyy', })}`;
+    nuevoAsientoRef = nuevoAsientoRef + `&Leyenda=${Leyenda}&TipoAsiento=${TipoAsiento}`;
   }
   return nuevoAsientoRef;
 }
