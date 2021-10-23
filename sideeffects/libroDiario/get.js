c0de5b6ad6290ab45c61ae33a24001e6be427d7f
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-10-22 23:40:38 -0300

    Two decimal in libro diario

--- sideeffects/libroDiario/get.js
+++ sideeffects/libroDiario/get.js
@@ -1,11 +1,27 @@
-import { all, put, takeLatest, select, take } from 'redux-saga/effects';
+import { all, put, takeLatest } from 'redux-saga/effects';
 import {
   getLibroDiario,
   getLibroDiarioSuccess,
   getLibroDiarioError,
 } from '../../store/libroDiario';
-import { calcularBalance } from '../diarioMayor/get';
 import { formatDate } from '../../helpers/dates';
+import { newDecimal2 } from '../../helpers/decimalNumbers';
+
+const convertLibroDiarioToUi = (data) => {
+  return {
+    ...data,
+    asientos: data.asientos.map((asiento) => ({
+      ...asiento,
+      Renglones: asiento.Renglones.map((renglon) => ({
+        ...renglon,
+        Debe: newDecimal2(renglon.Debe),
+        Haber: newDecimal2(renglon.Haber),
+      })),
+    })),
+    TotalDebe: newDecimal2(data.TotalDebe),
+    TotalHaber: newDecimal2(data.TotalHaber),
+  }
+}
 
 function* getLibro({ payload: { Fecha, db } }) {
   let url = `http://localhost:3000/api/libroDiario/get-libro-diario?db=${db}`;
@@ -20,7 +36,7 @@ function* getLibro({ payload: { Fecha, db } }) {
   if (data.errorMessage) {
     yield put(getLibroDiarioError(data.errorMessage));
   } else {
-    yield put(getLibroDiarioSuccess(data))
+    yield put(getLibroDiarioSuccess(convertLibroDiarioToUi(data)))
   }
 }
 
