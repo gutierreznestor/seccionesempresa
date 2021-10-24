ba70ac6f3a6bc99639abe4e7ae9f0109fa61f2a2
Gabriel Gutierrez <ngabrielgutierrez@gmail.com>
2021-10-24 17:17:58 -0300

    getTitulosBalance helper

--- pages/api/balance/helper/getTitulosBalance.js
+++ pages/api/balance/helper/getTitulosBalance.js
@@ -1,15 +1,16 @@
 import { query } from '../../../../lib/db';
 
-const getCuentasBalance = async ({ db }) => {
+const getTitulosBalance = async ({ db }) => {
   const cuentas = await query(`
     SELECT 
       plan_cuentas.idPlanCuenta, 
       plan_cuentas.CodigoPlan, 
       plan_cuentas.Nombre
     FROM plan_cuentas
+    WHERE plan_cuentas.Tipo = 0
     ORDER BY plan_cuentas.CodigoPlan DESC
     `, null, db);
   return cuentas;
 };
 
-export default getCuentasBalance;
+export default getTitulosBalance;
